9ce10a2b05c43ed37f40d370552790673fe5a42d
Gideon Le Grange <gideon@legrange.me>
2021-06-23 13:00:29 +0200

    Corrected typo

--- src/main/java/me/legrange/sql/Driver.java
+++ src/main/java/me/legrange/sql/Driver.java
@@ -2,7 +2,7 @@ package me.legrange.sql;
 
 public interface Driver {
 
-     String getDatabaseName(Database database);
+    String getDatabaseName(Database database);
 
     String getTableName(Table table);
 
